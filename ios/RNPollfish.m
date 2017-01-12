0adc56e3436be2a57b4809b1b72bc5341e400a70
Ben Yee <ben@squaretwo.co>
2017-01-12 00:16:01 -0800

    add requestUUID to initialize

--- ios/RNPollfish.m
+++ ios/RNPollfish.m
@@ -16,14 +16,15 @@ RCT_EXPORT_MODULE()
 #pragma mark exported methods
 
 // Initialize Pollfish
-RCT_EXPORT_METHOD(initialize :(NSString *)apiKey :(BOOL *)debugMode  :(BOOL *)autoMode)
+RCT_EXPORT_METHOD(initialize :(NSString *)apiKey :(BOOL *)debugMode  :(BOOL *)autoMode :(NSString *)userId)
 {
     NSLog(@"initialize Pollfish");
     [Pollfish initAtPosition: PollFishPositionMiddleRight
                  withPadding: 0
              andDeveloperKey: apiKey
                andDebuggable: debugMode
-               andCustomMode: autoMode];
+               andCustomMode: autoMode
+               andRequestUUID: userId];
 }
 
 
